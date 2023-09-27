ea18485c5f94443f70fa2a671891241c4a740039
Trevor Foster <trevafoser@gmail.com>
2023-09-26 20:34:24 -0500

    Roughly outline header

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,8 +1,27 @@
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  return <></>;
+  return (
+    <div>
+      <header className="header">
+        <div className="header__logo">
+          <div>
+            <img src="/images/logo.svg" alt="wtwr logo" />
+          </div>
+          <div>Date</div>
+        </div>
+        <div className="header__avatar-logo">
+          <div>
+            <button type="text"> Add New Clothes </button>
+          </div>
+          <div>Name</div>
+          <div>
+            <img src="/images/avatar.svg" alt="wtwr logo" />
+          </div>
+        </div>
+      </header>
+    </div>
+  );
 }
 
 export default App;
