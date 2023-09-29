9dcb56c63247626c908cad214bfeef7caa75b04c
Trevor Foster <trevafoser@gmail.com>
2023-09-29 16:34:29 -0500

    Complete WeatherCard.js revisions

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -36,3 +36,66 @@ export const defaultClothingItems = [
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/wtwr-project/Coat.png?etag=298717ed89d5e40b1954a1831ae0bdd4",
   },
 ];
+
+export const weatherOptions = [
+  {
+    url: require("../images/day/sunny-day.svg").default,
+    day: true,
+    type: "sunny",
+  },
+  {
+    url: require("../images/day/cloudy-day.svg").default,
+    day: true,
+    type: "cloudy",
+  },
+  {
+    url: require("../images/day/foggy-day.svg").default,
+    day: true,
+    type: "foggy",
+  },
+  {
+    url: require("../images/day/rainy-day.svg").default,
+    day: true,
+    type: "rainy",
+  },
+  {
+    url: require("../images/day/snowy-day.svg").default,
+    day: true,
+    type: "snowy",
+  },
+  {
+    url: require("../images/day/stormy-day.svg").default,
+    day: true,
+    type: "stormy",
+  },
+  {
+    url: require("../images/night/sunny-night.svg").default,
+    day: false,
+    type: "sunny",
+  },
+  {
+    url: require("../images/night/cloudy-night.svg").default,
+    day: false,
+    type: "cloudy",
+  },
+  {
+    url: require("../images/night/foggy-night.svg").default,
+    day: false,
+    type: "foggy",
+  },
+  {
+    url: require("../images/night/rainy-night.svg").default,
+    day: false,
+    type: "rainy",
+  },
+  {
+    url: require("../images/night/snowy-night.svg").default,
+    day: false,
+    type: "snowy",
+  },
+  {
+    url: require("../images/night/stormy-night.svg").default,
+    day: false,
+    type: "stormy",
+  },
+];
