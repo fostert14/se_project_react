86e80438404b400d6fef3476426ca07b3450a153
Trevor Foster <trevafoser@gmail.com>
2024-05-13 15:37:09 -0500

    Add: Name to footer :wq

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,7 +2,7 @@ import "./Footer.css";
 
 const Footer = () => (
   <footer className="footer">
-    <div>Developed by Practicum Students</div>
+    <div>Developed by Trevor Foster</div>
     <div>2023</div>
   </footer>
 );
